126b01b5b4d21a844215c49c482a253821864147
joaquincorimayo <joaquin97corimayo@gmail.com>
2022-04-22 17:42:14 -0300

    Estructura del proyecto.

---
+++ TP3_Grupo10/src/main/java/ar/edu/unju/fi/tp3/Tp3Grupo10Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tp3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp3Grupo10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp3Grupo10Application.class, args);
+	}
+
+}
---
+++ TP3_Grupo10/src/test/java/ar/edu/unju/fi/tp3/Tp3Grupo10ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tp3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp3Grupo10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
