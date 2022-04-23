25ec49110fac435311392330176d29ef4861ea97
fv322 <38471258@fi.unju.edu.ar>
2022-04-23 19:39:48 -0300

    Desarrollo de actividad 3.

---
+++ TP3_Grupo10/src/main/java/ar/edu/unju/fi/ejercicio3/Alumno.java
@@ -0,0 +1,86 @@
+package ar.edu.unju.fi.ejercicio3;
+
+import java.util.Arrays;
+
+public class Alumno {
+	String nombre;
+	String apellido;
+	int libretaUniversitaria;
+	double[] notas;
+
+	// CONSTRUCTORES
+	public Alumno() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Alumno(String nombre, String apellido, int libretaUniversitaria, double[] notas) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.libretaUniversitaria = libretaUniversitaria;
+		this.notas = notas;
+	}
+
+	// GET Y SET DE LOS ATRIBUTOS
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public int getLibretaUniversitaria() {
+		return libretaUniversitaria;
+	}
+
+	public void setLibretaUniversitaria(int libretaUniversitaria) {
+		this.libretaUniversitaria = libretaUniversitaria;
+	}
+
+	public double[] getNotas() {
+		return notas;
+	}
+
+	public void setNotas(double[] notas) {
+		this.notas = notas;
+	}
+
+	
+
+	@Override
+	public String toString() {
+		return "Nombre del Alumno=" + nombre + ", Apellido del Alumno=" + apellido + ", Libreta Universitaria del Alumno=" + libretaUniversitaria
+				+ ", Notas del Alumno=" + Arrays.toString(notas);
+	}
+
+	public double calcularPromedio() {
+		double suma = 0.0;
+		double promedio = 0.0;
+		for (int i = 0; i < notas.length; i++) {
+			suma += notas[i];
+		}
+		promedio = suma / notas.length;
+		return promedio;
+	}
+
+	public double obtenerNotaMaxima() {
+		double max = 0;
+		for (int i = 0; i < notas.length; i++) {
+			if (notas[i] > max) {
+				max = notas[i];
+			}
+		}
+		return max;
+	}
+
+}
+
