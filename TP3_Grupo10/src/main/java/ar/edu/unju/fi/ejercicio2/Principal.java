29576725d13a9c76913a4f13ea8419be52b62cbd
joaquincorimayo <joaquin97corimayo@gmail.com>
2022-04-24 15:10:50 -0300

    Se agrego control de ingreso.

--- TP3_Grupo10/src/main/java/ar/edu/unju/fi/ejercicio2/Principal.java
+++ TP3_Grupo10/src/main/java/ar/edu/unju/fi/ejercicio2/Principal.java
@@ -13,26 +13,27 @@ public class Principal {
 
 		try {
 			while (band) {
-				System.out.println("Ingrese un numero entero entre [1,9] ");
+				System.out.print("Ingrese un numero entero entre [1,9]: ");
 				ingreso = scan.nextInt();
-				for (int i : num) {
-					int x = ingreso * i;
-					System.out.println(ingreso + "x" + i + "=" + x);
-				}
-				System.out.println("Desea volver a intentar? ");
-				System.out.println("S/N");
-				String opc = scan.next();
-				if (opc.equals("s") || opc.equals("S")) {
-					band = true;
+				if (ingreso >= 1 && ingreso <= 9) {
+					for (int i : num) {
+						int x = ingreso * i;
+						System.out.println(ingreso + "x" + i + "=" + x);
+					}
+					System.out.println("Desea volver a intentar <s> o salir del programa <n>");
+					System.out.print("Ingresar respuesta: ");
+					String opc = scan.next();
+					if (opc.equals("s") || opc.equals("S")) {
+						band = true;
+					} else {
+						band = false;
+						System.out.println("FIN DEL PROGRAMA");
+					}
 				} else {
-
-					band = false;
-					System.out.println("FIN DEL PROGRAMA");
-
+					System.out.println("Fuera del rango permitido. Intentar nuevamente.");
 				}
 			}
 		}
-
 		catch (InputMismatchException e) {
 			System.out.println("DEBE INGRESAR UN NUMERO ENTERO");
 		}
